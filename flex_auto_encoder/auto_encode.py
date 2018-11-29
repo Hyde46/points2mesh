569e48560625df7b8bdb5f32bf9ffcbdccf2c5b5
Hyde46 <denis.heid@student.uni-tuebingen.de>
2018-11-29 16:55:15 +0100

    Add First flex conv autoencoder tests

--- flex_auto_encoder/auto_encode.py
+++ flex_auto_encoder/auto_encode.py
@@ -5,55 +5,87 @@ import numpy as np
 import cv2
 
 from tensorpack import *
-from layers import flex_convolution, flex_pooling, knn_bruteforce
+from layers import (flex_convolution, flex_pooling, knn_bruteforce, flex_convolution_transpose)
+from PointCloudDataFlow import *
 
-enable_argscope_for_module(tf.payers)
+enable_argscope_for_module(tf.layers)
 
 TOTAL_BATCH_SIZE = 16
-BATCH_SIZE = 16
+BATCH_SIZE = 64
 
 
 PC = {'num': 1024, 'dp':3}
 
+
+
 class Model(ModelDesc):
+
     def inputs(self):
-        reuturn [tf.placeholder(tf.float32, (None, PC['dp'], PC['num']), 'positions']
+        """
+        Inputs are
+        - pointcloud [dbatch,im_position, num_points]
+        - Input should be equal to output if autoencoder is working nicely
+        """
+        #return [tf.placeholder(tf.float32, (PC['num'], PC['dp'], None), 'positions')]
+        return [tf.placeholder(tf.float32, (None, PC['dp'], PC['num']), 'positions')]
                 
     def build_graph(self, positions):
-        print tf.shape(positions)
-        features = positions
-        neighbors = _knn_bruteforce(positions, K=8 )
-
-        x = features
+        """
+        Autoencoder
+        """
+        net = positions
+        pos = positions
+        neighbors = knn_bruteforce( pos, K=8 )
+        #flat = tf.layers.flatten(x)
+        ### Encoding
+        #encoding = tf.layers.dense(flat, units=100) 
+        #decoding = tf.layers.dense(encoding, units=flat.shape[-1]) 
+        #total_cost = tf.reduce_sum(tf.square(tf.subtract(flat,decoding,name="difference")))
+        #return total_cost
 
         def subsample(x):
             n = x.shape.as_list()[-1]
-            return x[:, :, :n // 4]
+            return x[: , :,  : n // 2]
         def upsample (x):
             """
             Super simple upsampling
             """
-            return tf.concat([x,x],0) 
+            return tf.concat([x,x], axis = 2)
+        
+        net = flex_convolution(net, pos, neighbors, 32, name ="Conv32", activation=tf.nn.relu)
+        net = flex_convolution(net, pos, neighbors, 16, name ="Conv16", activation=tf.nn.relu)
+        net = flex_convolution(net, pos, neighbors, 8, name="Conv8", activation=tf.nn.relu)
+        net = flex_convolution(net, pos, neighbors, 3, name='reconstructed_points')
+        '''
+        with tf.name_scope("Encoder"):
+            net = flex_convolution(net, pos, neighbors, 8, name="Conv8", activation=tf.nn.relu)
+            net = flex_pooling(net, neighbors)
+            net = subsample(net)
+            pos = subsample(pos)
+            neighbors = knn_bruteforce(pos, K=8)
+            net = flex_convolution(net, pos, neighbors, 16, name="Conv16", activation=tf.nn.relu)
+            net = flex_pooling(net, neighbors)
+            net = subsample(net)
+            pos = subsample(pos)
+            neighbors = knn_bruteforce(pos, K=8)
+            net = flex_convolution(net, pos, neighbors, 32, name="Conv32", activation=tf.nn.relu)
 
-        # Encoder
-        for stage in range(3):
-            x = flex_convolution(x, positions, neighbors, 64 / ((1 +  stage ) * 2), activation=tf.nn.relu)
-            x = flex_pooling(x, neighbors)
-            x = subsample(x)
-            positions = subsample(positions)
-            neighbors = knn_bruteforce(positions, K=8 )
+        with tf.name_scope ("Decoder"):
+            net = flex_convolution_transpose(net, pos, neighbors, 16, name="Deconv16", activation=tf.nn.relu)
+            net = flex_pooling(net, neighbors)
+            net = upsample(net)
+            pos = upsample(pos)
+            neighbors = knn_bruteforce(pos, K=8)
+            net = flex_convolution_transpose(net, pos, neighbors, 8, name="Deconv8", activation=tf.nn.relu)
+            net = flex_pooling(net, neighbors)
+            net = upsample(net)
+            pos = upsample(pos)
+            neighbors = knn_bruteforce(pos, K=8)
 
-        # Decoder
-        for stage in range(3):
-            x = flex_convolution(x, positions, neighbors, 4 * ((1 + stage) * 2), activation=tf.nn.relu)
-            x = flex_pooling(x, neighbors)
-            x = upsample(x)
-            positions = upsample(positions)
-            neighbors = knn_bruteforce(positions, K= 8)
-        r = flex_convolution(x, positions, neighbors, 1, activation=tf.nn.sigmoid)
-        print tf.shape(r)
-        # axis ? 
-        total_cost = tf.reduce_sum(tf.math.subtract(positions,r,name="difference"))
+            net = flex_convolution_transpose(net, pos, neighbors, 3, name='reconstructed_points')
+'''
+        total_cost = tf.reduce_mean(tf.square(tf.subtract(net,positions)),name="square_difference")
+        summary.add_moving_summary(total_cost)
         return total_cost
 
 
@@ -61,34 +93,34 @@ class Model(ModelDesc):
         return tf.train.AdamOptimizer(1e-4)
 
 
-def get_data():
-    df_train = []
-
-    df_test = []
-    return df_train, df_test
-
-
 if __name__ == '__main__':
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--gpu', help='comma separated list of GPU(s) to use.')
-  parser.add_argument('--load', help='load model')
-  parser.add_argument('--fusion', help='run sampling', default='',
-                      choices=['pooling', 'conv'])
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', help='comma separated list of GPU(s) to use.')
+    parser.add_argument('--load', help='load model')
+    parser.add_argument('--fusion', help='run sampling', default='') 
+    args = parser.parse_args()
 
-  if args.gpu:
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
-  logger.set_logger_dir('train_log/fusion_%s' % (args.fusion))
+    logger.set_logger_dir('train_log/fusion_%s' % (args.fusion))
 
-  dataset_train, dataset_test = get_data()
-  steps_per_epoch = len(dataset_train)
-
-  config = TrainConfig(
+    #Loading Data
+    df_train = get_point_cloud_dataflow('train',batch_size=BATCH_SIZE,num_points=PC["num"], model_ver="10",shuffle=False, normals=False)
+    df_test = get_point_cloud_dataflow('test',batch_size=2*BATCH_SIZE,num_points=PC["num"], model_ver="10",shuffle=False, normals=False)
+    steps_per_epoch = len(df_train)
+    #Setup training step
+    config = TrainConfig(
         model=Model(),
-        data=FeedInput(dataset_train),
+        #data=FeedInput(df_train),
+        dataflow = df_train,
+        callbacks=[
+            ModelSaver(),
+            InferenceRunner(df_test, ScalarStats(['square_difference'])),
+            ],
         steps_per_epoch=steps_per_epoch,
-        max_epoch=100
+        max_epoch=10,
+        session_init=SaverRestore(args.load) if args.load else None
           )
-  launch_train_with_config(config, SimpleTrainer())
+    launch_train_with_config(config, SimpleTrainer())
