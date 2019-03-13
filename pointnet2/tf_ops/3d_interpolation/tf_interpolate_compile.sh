8595057a072b044fb77bcaadffba893a47668570
Hyde46 <denis.heid@student.uni-tuebingen.de>
2019-03-13 13:50:55 +0100

    Fix compile script

--- pointnet2/tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ pointnet2/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,5 +1,5 @@
 # TF1.2
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # TF1.4
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/heid/.local/lib/python2.7/site-packages/tensorflow/include -I /graphics/opt/opt_Ubuntu18.04/cuda/toolkit_10.0/cuda/include -I /home/heid/.local/lib/python2.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /graphics/opt/opt_Ubuntu18.04/cuda/toolkit_10.0/cuda/lib64/ -L/home/heid/.local/lib/python2.7/site-packages/tensorflow -ltensorflow_framework -O2 
