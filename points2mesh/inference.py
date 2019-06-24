9142360f5dfbb6856a55bdd649abadd62c8155fd
Hyde46 <denis.heid@student.uni-tuebingen.de>
2019-06-24 16:25:04 +0200

    More results

--- points2mesh/inference.py
+++ points2mesh/inference.py
@@ -15,7 +15,7 @@ seed = 1024
 np.random.seed(seed)
 tf.set_random_seed(seed)
 
-PC = {'num': 1024, 'dp': 3, 'ver': "40"}
+PC = {'num': 7500, 'dp': 3, 'ver': "40"}
 # settings
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -24,21 +24,21 @@ flags.DEFINE_string(
 flags.DEFINE_float('learning_rate', 3e-5, 'Initial learning rage.')
 flags.DEFINE_integer('hidden', 192, 'Number of units in hidden layer')
 flags.DEFINE_integer(
-    'feat_dim', 227, 'Number of units in FlexConv Feature layer')
+    'feat_dim', 239, 'Number of units in FlexConv Feature layer')
 flags.DEFINE_integer('feature_depth', 32,
                      'Dimension of first flexconv feature layer')
 flags.DEFINE_integer('coord_dim', 3, 'Number of units in output layer')
 flags.DEFINE_float('weight_decay', 5e-6, 'Weight decay for L2 loss.')
 flags.DEFINE_float('collapse_epsilon', 0.008, 'Collapse loss epsilon')
-flags.DEFINE_integer('pc_num', 1024, 'Number of points per pointcloud object')
+flags.DEFINE_integer('pc_num', 7500, 'Number of points per pointcloud object')
 flags.DEFINE_integer('dp', 3, 'Dimension of points in pointcloud')
 flags.DEFINE_integer(
     'num_neighbors', 6, 'Number of neighbors considered during Graph projection layer')
 flags.DEFINE_integer('batch_size', 1, 'Batchsize')
-#flags.DEFINE_string('base_model_path', 'utils/ellipsoid/info_ellipsoid.dat',
-#                   'Path to base model for mesh deformation')
-flags.DEFINE_string('base_model_path', 'utils/ellipsoid/torus_small.dat',
-                    'Path to base model for mesh deformation')
+flags.DEFINE_string('base_model_path', 'utils/ellipsoid/info_ellipsoid.dat',
+                   'Path to base model for mesh deformation')
+#flags.DEFINE_string('base_model_path', 'utils/ellipsoid/torus_small.dat',
+#                    'Path to base model for mesh deformation')
 #flags.DEFINE_string('base_model_path', 'utils/ellipsoid/ellipsoid.dat',
 #                    'Path to base model for mesh deformation')
 
@@ -46,8 +46,8 @@ num_blocks = 3
 num_supports = 2
 
 
-def noise_augment(data, noise_level=0.01):
-    rnd = np.random.rand(3, 1024)*2*noise_level - noise_level
+def noise_augment(data, noise_level=0.00):
+    rnd = np.random.rand(3, 7500)*2*noise_level - noise_level
     return data + rnd
 
 def load_pc(pc_path, num_points):
@@ -99,7 +99,7 @@ def predict(predictor, data, path):
 
 def loadModel():
     prediction = PredictConfig(
-        session_init=get_model_loader("/graphics/scratch/students/heid/train_log/fusion_c3_noise_1024_big_/checkpoint"),
+        session_init=get_model_loader("/graphics/scratch/students/heid/train_log/fusion_c1_7500_big_/checkpoint"),
         model=FlexmeshModel(PC, name="Flexmesh"),
         input_names=['positions'],
         output_names=['mesh_outputs/output1',
@@ -119,6 +119,7 @@ def loadTxtFiles(path):
 
 
 categories =["airplane","bed","bottle","bowl","car","chair","guitar","toilet","person","bathtub"]
+categories = ["person","bathtub"]
 #categories =["person","bathtub","sofa"]
 #path = "/home/heid/Documents/master/pc2mesh/point_cloud_data/small/"
 #path = "/home/heid/Documents/master/pc2mesh/point_cloud_data/evaluation_set/single_class"
@@ -147,12 +148,12 @@ for c in categories:
 
     path = "/graphics/scratch/students/heid/evaluation_set/"+c
     pcs = loadTxtFiles(path)
-    path_output = "/graphics/scratch/students/heid/inference/c1_noise_1024_"+c
+    path_output = "/graphics/scratch/students/heid/inference/c3_7500_"+c
     counter = 0 
     for pc in pcs:
 
         path_pc = os.path.join(path, pc)    
-        pc_inp = load_pc(path_pc, num_points=1024)
+        pc_inp = load_pc(path_pc, num_points=7500)
         vertices = predict(predictor, pc_inp, path_pc)
         create_inference_mesh(vertices[2], 3, pc,
                             path_pc, path_output, display_mesh=False, num_obj=counter)
